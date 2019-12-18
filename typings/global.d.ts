17e036109d041c48481af0952b35bd5804fe62c4
Cameron Diver <cameron@balena.io>
2019-12-18 15:52:42 +0000

    Add UnwrappedPromise and DeepPartial types
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -5,3 +5,9 @@ interface Dictionary<T> {
 type Callback<T> = (err?: Error, res?: T) => void;
 
 type Nullable<T> = T | null | undefined;
+
+type UnwrappedPromise = T extends PromiseLike<infer U> ? U : T;
+
+type DeepPartial<T> = T extends object
+	? { [K in keyof T]?: DeepPartial<T[K]> }
+	: T;
